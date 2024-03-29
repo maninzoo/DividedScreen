56f27c51fadb883d32da6f921ac9a6e7146b9450
YoungSoo Choi <maninzoo@inswave.com>
2024-03-29 12:50:54 +0900

    Communicatoin between WebView

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.Wpf;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 
 namespace DividedScreen
 {
@@ -20,11 +13,57 @@ namespace DividedScreen
     /// </summary>
     public partial class MainWindow : Window
     {
+        Dictionary<string, WebView2> webViewList = new Dictionary<string, WebView2>();
+        
         public MainWindow()
         {
             InitializeComponent();
 
             webView1st.Source = new Uri("http://127.0.0.1:5500/index.html");
+
+            InitializeAsync();
+        }
+
+        async void InitializeAsync()
+        {
+            await webView1st.EnsureCoreWebView2Async(null);
+            await webView2nd.EnsureCoreWebView2Async(null);
+
+            webView1st.CoreWebView2.WebMessageReceived += webView1st_WebMessageReceived;
+            webView2nd.CoreWebView2.WebMessageReceived += webView2nd_WebMessageReceived;
+
+            webViewList.Add("webView1st", webView1st);
+            webViewList.Add("webView2nd", webView2nd);
+        }
+
+        private void webView1st_WebMessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            //Console.WriteLine("webView1st_WebMessageReceived : " + e.WebMessageAsJson);
+
+            runMessage(JObject.Parse(e.WebMessageAsJson));
+        }
+
+        private void webView2nd_WebMessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            //Console.WriteLine("webView2nd_WebMessageReceived : " + e.WebMessageAsJson);
+
+            runMessage(JObject.Parse(e.WebMessageAsJson));
+        }
+
+        private async void runMessage( JObject message )
+        {
+            var target = (string)message["target"];
+            var action = (string)message["action"];
+
+            if (action.Equals("navigation"))
+            {
+                //webView2nd.Source = new Uri((string)message["uri"]);
+                webViewList[target].Source = new Uri((string)message["uri"]);
+            }
+            else if (action.Equals("select"))
+            {
+                var result = await webViewList[target].CoreWebView2.ExecuteScriptAsync($@"selectCoffee('{(string)message["coffee"]}', '{target}');");
+            }
         }
     }
 }
